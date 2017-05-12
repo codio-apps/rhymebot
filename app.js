159c1fe7519dc547fe9ce7117eb23aa9b6249e8e
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-12 13:08:17 +0100

    Changes

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var express = require('express');
-var app = express();
+const
+  bodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),
+  request = require('request');
 
 
 
