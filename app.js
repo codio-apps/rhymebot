5355cd6f09328155c8400eb42bd8bd0c9266d712
Adam Stevens <ajstevens@ajstevens-mbp.local>
2017-05-12 13:16:17 +0100

    Update

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const
 
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  var app = express();
+  app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
